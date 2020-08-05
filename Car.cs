398343b5cffa03b35b3c06b5b9d7eb3f5662beba
Sandhaya Patel <56769455+Atharvpatel21@users.noreply.github.com>
2020-08-05 19:08:59 +0200

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace elkcar
+{
+	//class Car
+	//{
+	//}
+	public class Cars
+	{
+		public Cars(String make, int price)
+		{
+			Make = make;
+			Price = price;
+
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+		public string Make
+		{
+			get; set;
+		}
+
+		public int Price
+		{
+			get; set;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Elasticsearch.Net;
+
+namespace elkcar
+{
+    //class Program
+    //{
+    //    //static void Main(string[] args)
+    //    //{
+    //    //    Console.WriteLine("Hello World!");
+    //    //}
+        class Program
+        {
+            static async System.Threading.Tasks.Task Main(string[] args)
+            {                 
+                var settings = new ConnectionConfiguration(new Uri("http://localhost:9200/"))
+                .RequestTimeout(TimeSpan.FromMinutes(2));
+
+                var client = new ElasticLowLevelClient(settings);
+                var cars = new Cars("Ford", 1000000);
+            //var cars = new Cars("Ford", 200000);
+            var response = await client.IndexAsync<StringResponse>("cars", "1", PostData.Serializable(cars));
+                 string responseString = response.Body;
+                 Console.WriteLine(responseString);
+            }
+        }
+    
+}
+    
